736cbc3c14c1963cd49e561cf53dd49223e168d4
liyujie <liyujie_jack@163.com>
2017-08-25 10:50:18 +0800

    my-project
    this is my project

---
+++ src/components/componentA.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="hello">
+		<h1>{{msg}}</h1>
+		<h1>{{msgfromfather}}</h1>
+		<button @click="onClickMe">Click!</button>
+	</div>
+</template>
+
+<script>
+export default{
+	data: function() {
+		return{
+			msg: 'from component a'
+		}
+	},
+	props: ['msgfromfather'],
+	methods: {
+		onClickMe: function(){
+			this.$emit('child-tell-me-something',this.msg);
+		}
+	}
+}
+</script>
+
+<style>
+	h1{
+		color: #42B983;
+	}
+</style>
